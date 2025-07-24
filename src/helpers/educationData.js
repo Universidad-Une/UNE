0f910d8f4e04774282519f6674a7633fdcf738bd
Eliezer solano <develiezermartinez@gmail.com>
2025-07-23 18:14:57 -0600

    avances miercoles

---
+++ src/helpers/educationData.js
@@ -0,0 +1,157 @@
+// educationData.js
+
+export const educationData = {
+  primaria: {
+    programas: ["Primaria"],
+    planteles: ["PLAZA DEL SOL"],
+    modalidades: ["Escolarizada"]
+  },
+  secundaria: {
+    programas: ["Secundaria"],
+    planteles: [
+      "Av. México",
+      "PLAZA DEL SOL",
+      "TESISTÁN",
+      "TLAJOMULCO",
+      "TLAQUEPAQUE",
+      "TONALÁ"
+    ],
+    modalidades: ["Escolarizada"]
+  },
+  preparatoria: {
+    programas: ["BIS", "Bachillerato General", "Bachillerato en Línea"],
+    planteles: [
+      "Av. México",
+      "CARACOL",
+      "CENTRO",
+      "CENTRO MÉDICO",
+      "CHAPULTEPEC",
+      "MILENIO",
+      "PLAZA DEL SOL",
+      "TEPATITLÁN",
+      "TESISTÁN",
+      "TLAJOMULCO",
+      "TLAQUEPAQUE",
+      "TONALÁ",
+      "UNE EN LÍNEA",
+      "ZAPOPAN"
+    ],
+    modalidades: ["Escolarizada", "Semiescolarizada"]
+  },
+  tecnico: {
+    programas: [],
+    planteles: [],
+    modalidades: []
+  },
+  licenciatura: {
+    programas: [
+      "Abogado",
+      "Administración",
+      "Administración Hospitalaria",
+      "Administración de Empresas",
+      "Administración de Negocios",
+      "Administración de Recursos Humanos",
+      "Administración de Sistemas de Información",
+      "Administración de negocios",
+      "Alta Dirección",
+      "Arquitectura",
+      "Ciencias de la Comunicación",
+      "Ciencias de la Educación",
+      "Ciencias penales",
+      "Cirujano Dentista",
+      "Comercio y Negocios Globales",
+      "Comunicación",
+      "Contaduría",
+      "Contaduría Pública",
+      "Contaduría y Auditoria",
+      "Contaduría y Auditoría",
+      "Criminología y Criminalística",
+      "Cultura Física y del Deporte",
+      "Derecho",
+      "Derecho Laboral",
+      "Diseño Gráfico",
+      "Eduacación",
+      "Enfermería",
+      "Finanzas Estratégicas",
+      "Gastronomía",
+      "Gestión y Creación de PyME’s",
+      "Ing. Civil",
+      "Ing. En Desarrollo de Software",
+      "Ing. En Inteligencia de Datos y Ciberseguridad",
+      "Ing. Mecánica",
+      "Ingenieria Civil",
+      "Ingenieria Mecánica",
+      "Ingenieria de Datos y Ciberseguridad",
+      "Ingenieria en Comunicaciones y Electronica",
+      "Ingenieria en Desarrollo Software",
+      "Ingenieria en Desarrollo de Software",
+      "Ingenieria en Inteligencia de Datos y Ciberseguridad",
+      "Ingeniería Civil",
+      "Ingeniería Computación",
+      "Ingeniería Industrial",
+      "Ingeniería Química",
+      "Ingeniería en Comunicaciones y Electrónica",
+      "Licenciatura en Educación",
+      "Mercadotecnia",
+      "Mercadotecnia Internacional",
+      "Mtria. En Educación",
+      "Negocios Internacionales",
+      "Nutrición",
+      "Programa",
+      "Psicología",
+      "Químico Farmacéutico Biólogo",
+      "Terapia Familiar Sistémica",
+      "Terapia Familiar y Sistémica",
+      "Terapia Física",
+      "Trabajo Social"
+    ],
+    planteles: [
+      "Av. México",
+      "CAMPUS PUERTO VALLARTA",
+      "CARACOL",
+      "CENTRO",
+      "CENTRO MÉDICO",
+      "CHAPULTEPEC",
+      "LAS JUNTAS",
+      "MILENIO",
+      "PLAZA DEL SOL",
+      "TEPATITLÁN",
+      "TESISTÁN",
+      "TLAJOMULCO",
+      "TLAQUEPAQUE",
+      "TONALÁ",
+      "TORRE QUETZAL",
+      "TORRE UNE MILENIO",
+      "UNE EN LÍNEA",
+      "ZAPOPAN"
+    ],
+    modalidades: [
+      "Escolarizada",
+      "Escolarizado",
+      "Martes y Jueves",
+      "Modalidad",
+      "Sabatimo",
+      "Sabatina",
+      "Sabatino",
+      "Semiescolarizada",
+      "Viernes y Sabado",
+      "Viernes y Sábado",
+      "Viernes/Sábado"
+    ]
+  },
+  especialidad: {
+    programas: [],
+    planteles: [],
+    modalidades: []
+  },
+  maestria: {
+    programas: [],
+    planteles: [],
+    modalidades: []
+  },
+  doctorado: {
+    programas: [],
+    planteles: [],
+    modalidades: []
+  }
+};
